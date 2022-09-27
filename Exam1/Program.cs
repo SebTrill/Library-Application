cd0960124deced18f5811c067ee02c3c5127870a
zachmberard@gmail.com <zachmberard@gmail.com>
2022-09-26 21:33:44 -0500

    changes

--- Exam1/Program.cs
+++ Exam1/Program.cs
@@ -2,6 +2,9 @@ namespace Exam1
 {
     internal static class Program
     {
+
+        
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,7 +14,7 @@ namespace Exam1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new LibraryView());
+            Application.Run(new LibraryView(c));
         }
     }
 }
